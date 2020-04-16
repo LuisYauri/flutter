56c0f72a765adbb0c22b369306230303cecd7330
Luis Yauri <david.yauri147@gmail.com>
2020-04-16 07:52:01 -0500

    Finish flutter_app

--- flutter_app/lib/src/pages/contador_page.dart
+++ flutter_app/lib/src/pages/contador_page.dart
@@ -13,33 +13,65 @@ class _ContadorPageState extends State<ContadorPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Titulo'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hola Mundo',
-              style: _styleText,
-            ),
-            Text(
-              '$_conteo',
-              style: _styleText,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Titulo'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Hola Mundo',
+                style: _styleText,
+              ),
+              Text(
+                '$_conteo',
+                style: _styleText,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: _crearBotones());
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: _reset,
+        ),
+        Expanded(
+          child: SizedBox(width: 5.0),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: _sustraer,
+        ),
+        SizedBox(width: 5.0),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _agregar,
+        ),
+      ],
     );
   }
+
+  void _agregar() {
+    setState(() => _conteo++);
+  }
+
+  void _sustraer() {
+    setState(() => _conteo--);
+  }
+
+  void _reset() {
+    setState(() => _conteo = 0);
+  }
 }
+
+// Una función con () hace que se ejecute la función.
+// No ponemos el () para que no se ejecute.
