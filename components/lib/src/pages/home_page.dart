80ef1bbb3589605236ab831d2fc5132e46cb5778
Luis Yauri <david.yauri147@gmail.com>
2020-04-17 04:28:03 -0500

    Add Navigator traditional

--- components/lib/src/pages/home_page.dart
+++ components/lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/providers/menu_provider.dart';
+import 'package:components/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,26 +19,28 @@ class HomePage extends StatelessWidget {
       future: menuProvider.cargarData(),
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
     data.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
-        leading: Icon(
-          Icons.account_circle,
-          color: Colors.blue,
-        ),
+        leading: getIcon(opt['icon']),
         trailing: Icon(
           Icons.keyboard_arrow_right,
           color: Colors.blue,
         ),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(
+            builder: (context) => AlertPage(),
+          );
+          Navigator.push(context, route);
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });
