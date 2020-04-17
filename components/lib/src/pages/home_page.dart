2095d6a1f3bf0e4c98ca2bea2c3a8c1c00fe209a
Luis Yauri <david.yauri147@gmail.com>
2020-04-17 04:04:24 -0500

    Add future builder

--- components/lib/src/pages/home_page.dart
+++ components/lib/src/pages/home_page.dart
@@ -5,23 +5,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Components')),
+      appBar: AppBar(
+        title: Text('Components'),
+      ),
       body: _lista(),
     );
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Hola Mundo'),
-      ),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.account_circle,
+          color: Colors.blue,
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.blue,
+        ),
+        onTap: () {},
+      );
+      opciones..add(widgetTemp)..add(Divider());
+    });
+    return opciones;
   }
 }
