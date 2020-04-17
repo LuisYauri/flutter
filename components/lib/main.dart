7c51a5a68e38a96fac42dc41db627ab9d949673f
Luis Yauri <david.yauri147@gmail.com>
2020-04-16 21:09:27 -0500

    Add dart home

---
+++ components/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:components/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: HomePageTemp(),
+    );
+  }
+}
+
+// Usamos mateapp para crear un código modelo
