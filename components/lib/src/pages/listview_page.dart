d9bca93dd13f75a75ddd7a4412fbc8084863574d
Luis Yauri <david.yauri147@gmail.com>
2020-04-18 08:13:27 -0500

    Add scroll controller

--- components/lib/src/pages/listview_page.dart
+++ components/lib/src/pages/listview_page.dart
@@ -6,7 +6,22 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+  List<int> _listaNumeros = new List<int>();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,14 +34,25 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index) {
         final image = _listaNumeros[index];
         return FadeInImage(
           image: NetworkImage('https://picsum.photos/id/$image/500/300'),
           placeholder: AssetImage('assets/jar-loading.gif'),
+          height: 250,
+          fit: BoxFit.cover,
         );
       },
     );
+  }
+
+  void _agregar10() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
   }
 }
