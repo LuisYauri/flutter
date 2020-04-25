423b97895102ad4482569732cb6644e14021c568
Luis Yauri <david.yauri147@gmail.com>
2020-04-25 08:59:27 -0500

    finish design app

--- diseno_1/lib/main.dart
+++ diseno_1/lib/main.dart
@@ -1,17 +1,31 @@
+import 'dart:io' show Platform;
+
 import 'package:diseno1/pages/basico_page.dart';
+import 'package:diseno1/pages/botones_page.dart';
+import 'package:diseno1/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    if (Platform.isAndroid) {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+          .copyWith(statusBarColor: Colors.transparent));
+    } else if (Platform.isIOS) {
+      SystemChrome.setSystemUIOverlayStyle(
+          SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.white));
+    }
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basico',
+      initialRoute: 'botones',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
+        'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(),
       },
     );
   }
