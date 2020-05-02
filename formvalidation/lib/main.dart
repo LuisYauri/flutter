408e6b2fa09ff213d60250f1866b8a9c0abcb5a0
Luis Yauri <david.yauri147@gmail.com>
2020-05-02 09:01:48 -0500

    Update login finihs

--- formvalidation/lib/main.dart
+++ formvalidation/lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:formvalidation/src/bloc/provider.dart';
 import 'package:formvalidation/src/pages/home_page.dart';
 import 'package:formvalidation/src/pages/login_page.dart';
+import 'package:formvalidation/src/pages/producto_page.dart';
+import 'package:formvalidation/src/pages/registro_page.dart';
+import 'package:formvalidation/src/preferencias_usuario/preferencias_usuariop.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = new PreferenciasUsuario();
+  await prefs.initPrefs();
+  runApp(MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,7 +24,9 @@ class MyApp extends StatelessWidget {
         initialRoute: 'login',
         routes: {
           'login': (BuildContext context) => LoginPage(),
+          'registro': (BuildContext context) => RegistroPage(),
           'home': (BuildContext context) => HomePage(),
+          'producto': (BuildContext context) => ProductoPage(),
         },
         theme: ThemeData(
           primaryColor: Colors.deepPurple,
